64170ac7cfe6491160f3756a705998ac347ac429
aaron <924393366@qq.com>
2020-11-08 09:18:48 +0800

    update(sql):更新异常邮件概率查询sql语句

--- src/com/jeedt/sql/异常的邮件概率.sql
+++ src/com/jeedt/sql/异常的邮件概率.sql
@@ -39,8 +39,9 @@ date        p
 2020-01-12没有失败的情况，所以概率为0.000.
  */
 select e.date,
-round(1.0*sum(case e.type when 'no_completed' then 1 else 0 end)/count(*) ,3) p
+round(sum(case e.type when 'no_completed' then 1 else 0 end)/count(*) ,3) p
 from email e join user u1 on e.send_id=u1.id
 join user u2 on e.receive_id=u2.id
 where u1.is_blacklist=0 and u2.is_blacklist=0
 group by e.date
+order by e.date --mysql8.0不再支持group by的隐式或显示排序
